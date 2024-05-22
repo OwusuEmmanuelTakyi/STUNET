6f25a0e48f9dd956e852d5db172660c109e32a68
“OwusuEmmanuelTakyi” <owusuemmanueltakyi@gmail.com>
2024-05-22 08:40:03 +0000

    created the header component

---
+++ StuNet/Apps/Components/HomeScreen/Header.jsx
@@ -0,0 +1,19 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { useUser } from '@clerk/clerk-expo';
+
+
+export default function Header() {
+    const {user}=useUser();
+  return (  
+    <View className = "flex flex-row items-center gap-2">
+      <Image source = {{uri:user?.imageUrl}}
+      className = "rounded-full w-12 h-12"
+      />
+      <View>
+        <Text className="text-[16px]">Welcome</Text>
+        <Text className="text-[20px] font-bold">{user?.fullName}</Text>
+      </View>
+    </View>
+  )
+}
