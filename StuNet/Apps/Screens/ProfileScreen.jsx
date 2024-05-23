b6c357e2af439ab86dad078ee5b45b03c68fa72d
“OwusuEmmanuelTakyi” <owusuemmanueltakyi@gmail.com>
2024-05-23 13:03:01 +0000

    working on the profile screen

--- StuNet/Apps/Screens/ProfileScreen.jsx
+++ StuNet/Apps/Screens/ProfileScreen.jsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image} from 'react-native'
 import React from 'react'
+import { useUser } from '@clerk/clerk-expo';
 
 export default function ProfileScreen() {
+
+  const {user}=useUser();
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View className="p-5">
+      <View className="items-center mt-14">
+      <Image source = {{uri:user?.imageUrl}}
+       className ="w-[100px] h-[100px] rounded-full"
+      />
+      <Text className="font-bold text-[20px] mt-5 ">{user?.fullName}</Text>
+      <Text className="font-bold text-[18px] mt-3 text-gray-500">{user?.primaryEmailAddress?.emailAddress}</Text>
+      </View>
     </View>
   )
 }
